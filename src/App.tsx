552c615fc6462dc9efd44c5f191f947ab8b875f4
João Casagrande <pngbighouse@gmail.com>
2023-08-18 15:56:54 -0300

    todo list corrigida

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,18 @@ const App = () => {
     setList(newList)
   }
 
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+
+    for(let i in newList) {
+      if(newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+
+    setList(newList);
+  }
+
   return (
     <C.Container>
       <C.Area>
@@ -32,7 +44,11 @@ const App = () => {
         <AddArea onEnter={handleAddTask} />
 
         {list.map((item, index) => (
-          <ListItem key={index} item={item} />
+          <ListItem 
+            key={index} 
+            item={item} 
+            onChange={handleTaskChange}
+          />
         ))}
 
       </C.Area>
